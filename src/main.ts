88a77eab402dff36123bdaa58dbea7b2a32da50f
GJ Tiquia <gershom.tiquia@gmail.com>
2025-01-19 23:05:17 -0500

    refactor: separate into functions

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,135 @@
-import { Application, Graphics, Point } from "pixi.js";
+import { Application, Container, Graphics, Point, Ticker } from "pixi.js";
 
 declare global {
     var __PIXI_APP__: Application
 }
 
+class GlobalContext {
+    app: Application;
+
+    UNIT_LENGTH: number;
+    WORLD_SIZE: Point;
+
+    UPPER_BOUND_Y: number
+    LOWER_BOUND_Y: number;
+    LEFT_BOUND_X: number;
+    RIGHT_BOUND_X: number;
+
+    constructor(app: Application) {
+        this.app = app;
+
+        this.UNIT_LENGTH = 20;
+        this.WORLD_SIZE = new Point(15, 15);
+
+        this.UPPER_BOUND_Y = app.screen.height / 2 - this.WORLD_SIZE.y * this.UNIT_LENGTH / 2;
+        this.LOWER_BOUND_Y = app.screen.height / 2 + this.WORLD_SIZE.y * this.UNIT_LENGTH / 2;
+        this.LEFT_BOUND_X = app.screen.width / 2 - this.WORLD_SIZE.x * this.UNIT_LENGTH / 2;
+        this.RIGHT_BOUND_X = app.screen.width / 2 + this.WORLD_SIZE.x * this.UNIT_LENGTH / 2;
+    }
+}
+
 (async () => {
     const app = new Application();
-
-    // for DevTools
-    globalThis.__PIXI_APP__ = app;
+    globalThis.__PIXI_APP__ = app; // For Dev Tools
 
     await app.init({ background: "#1c1917", resizeTo: window });
 
     document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-    const UNIT_LENGTH = 20;
-    const WORLD_SIZE = new Point(15, 15);
-    const UNIT_TIME_INTERVAL_MS = 100;
+    const ctx = new GlobalContext(app);
+
+    let boundary = createBoundary(ctx);
+    app.stage.addChild(boundary);
+
+    let snake = createSnake(ctx);
+    app.stage.addChild(snake.container);
+
+    app.ticker.add((ticker) => {
+        snake.update(ctx, ticker);
+    });
+})();
+
+class Snake {
+    container: Container;
+    elapsedTime: number;
+
+    constructor(ctx: GlobalContext) {
+        this.container = this.createSnakeContainer(ctx);
+        this.elapsedTime = 0;
+    }
+
+    private createSnakeContainer(ctx: GlobalContext) {
+        const app = ctx.app;
+        const UNIT_LENGTH = ctx.UNIT_LENGTH;
+        const UPPER_BOUND_Y = ctx.UPPER_BOUND_Y;
+
+        let snakeHead = new Graphics()
+            .rect(0, 0, UNIT_LENGTH, UNIT_LENGTH)
+            .fill("#f5f5f4");
+
+        snakeHead.position.set(app.screen.width / 2, UPPER_BOUND_Y + UNIT_LENGTH / 2)
+        snakeHead.updateTransform({ pivotX: snakeHead.width / 2, pivotY: snakeHead.height / 2 });
+        snakeHead.label = "Snake Head";
+
+        let container = new Container();
+        container.addChild(snakeHead);
+
+        return container;
+    }
+
+    public update(ctx: GlobalContext, ticker: Ticker) {
+        const UNIT_TIME_INTERVAL_MS = 100;
+
+        const UNIT_LENGTH = ctx.UNIT_LENGTH;
+        const UPPER_BOUND_Y = ctx.UPPER_BOUND_Y;
+        const LOWER_BOUND_Y = ctx.LOWER_BOUND_Y;
+
+        this.elapsedTime += ticker.deltaMS;
+        if (this.elapsedTime > UNIT_TIME_INTERVAL_MS) {
+            this.elapsedTime = 0;
+
+            let snakeHead = this.container.getChildByLabel("Snake Head");
+            let { x, y } = snakeHead!.position;
+            y += UNIT_LENGTH; // Moves downwards!
+
+            if (y > LOWER_BOUND_Y - UNIT_LENGTH / 2)
+                y = UPPER_BOUND_Y + UNIT_LENGTH / 2;
+
+            snakeHead!.position.set(x, y);
+        }
+    }
+}
+
+function createSnake(ctx: GlobalContext) {
+
+    return new Snake(ctx);
+}
+
+function createBoundary(ctx: GlobalContext) {
 
     const BOUNDARY_RECT_THICKNESS = 10;
+    const BOUNDARY_RECT_COLOR = "#ff0000";
 
-    const UPPER_BOUND_Y = app.screen.height / 2 - WORLD_SIZE.y * UNIT_LENGTH / 2;
-    const LOWER_BOUND_Y = app.screen.height / 2 + WORLD_SIZE.y * UNIT_LENGTH / 2;
-    const LEFT_BOUND_X = app.screen.width / 2 - WORLD_SIZE.x * UNIT_LENGTH / 2;
-    const RIGHT_BOUND_X = app.screen.width / 2 + WORLD_SIZE.x * UNIT_LENGTH / 2;
+    const app = ctx.app;
+    const WORLD_SIZE = ctx.WORLD_SIZE;
+    const UNIT_LENGTH = ctx.UNIT_LENGTH;
+    const UPPER_BOUND_Y = ctx.UPPER_BOUND_Y;
+    const LOWER_BOUND_Y = ctx.LOWER_BOUND_Y;
+    const LEFT_BOUND_X = ctx.LEFT_BOUND_X;
+    const RIGHT_BOUND_X = ctx.RIGHT_BOUND_X;
 
     let upperBoundaryRect = new Graphics()
         .rect(0, 0, WORLD_SIZE.x * UNIT_LENGTH + 2 * BOUNDARY_RECT_THICKNESS, BOUNDARY_RECT_THICKNESS)
-        .fill("#ff0000")
+        .fill(BOUNDARY_RECT_COLOR)
     let lowerBoundaryRect = new Graphics()
         .rect(0, 0, WORLD_SIZE.x * UNIT_LENGTH + 2 * BOUNDARY_RECT_THICKNESS, BOUNDARY_RECT_THICKNESS)
-        .fill("#ff0000")
+        .fill(BOUNDARY_RECT_COLOR)
     let leftBoundaryRect = new Graphics()
         .rect(0, 0, BOUNDARY_RECT_THICKNESS, WORLD_SIZE.y * UNIT_LENGTH + 2 * BOUNDARY_RECT_THICKNESS)
-        .fill("#ff0000")
+        .fill(BOUNDARY_RECT_COLOR)
     let rightBoundaryRect = new Graphics()
         .rect(0, 0, BOUNDARY_RECT_THICKNESS, WORLD_SIZE.y * UNIT_LENGTH + 2 * BOUNDARY_RECT_THICKNESS)
-        .fill("#ff0000")
+        .fill(BOUNDARY_RECT_COLOR)
 
     upperBoundaryRect.position.set(
         app.screen.width / 2,
@@ -77,35 +170,11 @@ declare global {
     leftBoundaryRect.label = "Left Boundary Rect";
     rightBoundaryRect.label = "Right Boundary Rect";
 
-    app.stage.addChild(upperBoundaryRect);
-    app.stage.addChild(lowerBoundaryRect);
-    app.stage.addChild(leftBoundaryRect);
-    app.stage.addChild(rightBoundaryRect);
+    let container = new Container();
+    container.addChild(upperBoundaryRect);
+    container.addChild(lowerBoundaryRect);
+    container.addChild(leftBoundaryRect);
+    container.addChild(rightBoundaryRect);
 
-    let rect = new Graphics()
-        .rect(0, 0, UNIT_LENGTH, UNIT_LENGTH)
-        .fill("#f5f5f4");
-
-    rect.position.set(app.screen.width / 2, UPPER_BOUND_Y + UNIT_LENGTH / 2)
-    rect.updateTransform({ pivotX: UNIT_LENGTH / 2, pivotY: UNIT_LENGTH / 2 });
-
-    app.stage.addChild(rect);
-
-    let elapsedTime = 0;
-
-    app.ticker.add((ticker) => {
-
-        elapsedTime += ticker.deltaMS;
-        if (elapsedTime > UNIT_TIME_INTERVAL_MS) {
-            elapsedTime = 0;
-
-            let { x, y } = rect.position;
-            y += UNIT_LENGTH; // Moves downwards!
-
-            if (y > LOWER_BOUND_Y - UNIT_LENGTH / 2)
-                y = UPPER_BOUND_Y + UNIT_LENGTH / 2;
-
-            rect.position.set(x, y);
-        }
-    });
-})();
+    return container;
+}
